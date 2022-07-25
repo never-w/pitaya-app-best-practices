59fb0c0745839e9cc0064ed56b4829bf529c55d7
yinqiao <1737666820@qq.com>
2022-07-25 17:19:10 +0800

    feat: 新增文档架子

---
+++ .umirc.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'pitaya-app-best-practices',
+  favicon: 'https://avatars.githubusercontent.com/u/74942048',
+  logo: 'https://avatars.githubusercontent.com/u/74942048',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  locales: [['zh-CN', '中文']],
+});
---
+++ src/Foo/index.md
@@ -0,0 +1,16 @@
+---
+nav:
+  title: Components
+  path: /components
+---
+
+## Foo
+
+Demo:
+
+```tsx
+import React from 'react';
+import { Foo } from 'pitaya-app-practice';
+
+export default () => <Foo title="First Demo" />;
+```
